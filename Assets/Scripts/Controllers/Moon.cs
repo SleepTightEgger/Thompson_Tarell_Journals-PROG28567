2598f4cd98466163a721c621e0e90be174b6e510
Tarell <thomtare@shernet.sheridancollege.ca>
2025-10-10 18:06:41 -0400

    Task 3 - Orbital Moon
    Created method for moon orbiting a target.

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float oribitSpeed;
+    public float orbitRadius;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float angle;
 
-    // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(orbitRadius, oribitSpeed, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        angle += speed;
+        float angleCos = Mathf.Cos(angle * Mathf.Deg2Rad);
+        float angleSin = Mathf.Sin(angle * Mathf.Deg2Rad);
+        Vector2 moonPos = new Vector2(angleCos, angleSin) * radius;
+        transform.position = (Vector2)target.position + moonPos;
     }
 }
