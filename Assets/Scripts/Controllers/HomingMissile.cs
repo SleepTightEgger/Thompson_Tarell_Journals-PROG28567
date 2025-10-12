84e01a7f92b30eca58696aafde0ffc934948e79a
Tarell <thomtare@shernet.sheridancollege.ca>
2025-10-12 15:25:07 -0400

    Added more functionality. Calculation of direction to target and rotating in that direction.

--- Assets/Scripts/Controllers/HomingMissile.cs
+++ Assets/Scripts/Controllers/HomingMissile.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class HomingMissile : MonoBehaviour
 {
+    public Transform target;
+    public float angluarSpeed = 180f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,22 @@ public class HomingMissile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MissileMovement(target);
+    }
+
+    public void MissileMovement(Transform targetTransform)
+    {
+        Vector3 directionToTarget = (targetTransform.position - transform.position).normalized;
+
+        float directionAngle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+        float upAngle = Mathf.Atan2(transform.up.y, transform.up.x) * Mathf.Rad2Deg;
+
+        float deltaAngle = Mathf.DeltaAngle(upAngle, directionAngle);
+        float sign = Mathf.Sign(deltaAngle);
+
+        transform.Rotate(new Vector3(0, 0, angluarSpeed * Time.deltaTime * sign));
+
+        Debug.DrawLine(transform.position, transform.position + transform.up, Color.green);
+        Debug.DrawLine(transform.position, transform.position + directionToTarget, Color.red);
     }
 }
